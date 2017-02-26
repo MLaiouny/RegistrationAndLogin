9c6caa156990a6c03e51eaad9e25f96c78c87b68
MLaiouny <mlaiouny@gmail.com>
2017-02-25 22:35:57 -0500

    added the app.css file and moved the main.ts around

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,15 @@
 import {Routes, RouterModule} from '@angular/router';
 
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from './home.index';
 import {LoginComponent} from './login/index';
 import {RegisterComponent} from './register/index';
 import {AuthGuard} from './_guards/index';
-import {routing} from './app.routing';
+
 
 const appRoutes: Routes = [
 	{path: '', component: HomeComponent, canActivate: [AuthGuard]},
 	{path: 'login', component: LoginComponent},
-	{path: 'register', component: RegisterCompnent},
+	{path: 'register', component: RegisterComponent},
 	// Otherwise redirect to home
 	{path: '**', redirectTo: ''}
 ];
